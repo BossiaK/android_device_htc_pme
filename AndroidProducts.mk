88da4123c7997131494bba170e1d924a7ff9da8c
BossiaK Gaming <76888518+BossiaK@users.noreply.github.com>
2021-04-03 16:46:31 +0200

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/colt_pme.mk
+    $(LOCAL_DIR)/corvus_pme.mk
 
 COMMON_LUNCH_CHOICES := \
-    colt_pme-userdebug
+    corvus_pme-userdebug
