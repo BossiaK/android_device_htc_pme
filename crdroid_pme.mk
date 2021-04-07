3f1de396329ced1eef5816d70b7fbc9affdf7b2e
BossiaK Gaming <76888518+BossiaK@users.noreply.github.com>
2021-04-08 01:24:55 +0200

    Update and rename havoc_pme.mk to crdroid_pme.mk

--- crdroid_pme.mk
+++ crdroid_pme.mk
@@ -22,11 +22,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/havoc/config/common.mk)
+$(call inherit-product, vendor/crdroid/config/common.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := pme
-PRODUCT_NAME := havoc_pme
+PRODUCT_NAME := crdroid_pme
 PRODUCT_BRAND := HTC
 PRODUCT_MODEL := HTC 10
 PRODUCT_MANUFACTURER := HTC
@@ -39,5 +39,5 @@ $(call inherit-product-if-exists, vendor/htc/pme/pme-vendor.mk)
 BUILD_FINGERPRINT := htc/pmeuhl_00401/htc_pmeuhl:8.0.0/OPR1.170623.027/1012001.2:user/release-keys
   # For Device maintainer and CPU Information
   #PRODUCT_PROPERTY_OVERRIDES += \
-  #  ro.colt.maintainer=BossiaK \
-   # ro.colt.cpu=MSM8996
+  #  ro.crdroid.maintainer=BossiaK \
+   # ro.crdroid.cpu=MSM8996
